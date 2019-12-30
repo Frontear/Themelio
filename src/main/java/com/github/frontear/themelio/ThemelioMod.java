8efa1d3a3b58eb9562bd847d2d9615d976d9f145
Ali Rizvi <frontear4594@gmail.com>
2019-12-30 16:43:38 -0500

    Conform the environment to a strict standard
    A wiki article is being written up for how to follow this standard.

--- src/main/java/com/github/frontear/themelio/ThemelioMod.java
+++ src/main/java/com/github/frontear/themelio/ThemelioMod.java
@@ -4,7 +4,7 @@ import com.github.frontear.themelio.mixins.IMinecraftClient;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.client.MinecraftClient;
 
-public final class ExampleMod implements ModInitializer {
+public final class ThemelioMod implements ModInitializer {
     @Override
     public void onInitialize() {
         ((IMinecraftClient) MinecraftClient.getInstance()).getLogger().info("Hello Fabric!");
